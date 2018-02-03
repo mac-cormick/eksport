a86c5f1fb36486c3b78cd8ace5ae479c80372a86
home-mac <mac-cormick@yandex.ru>
2018-02-03 23:38:30 +0300

    done

--- script.js
+++ script.js
@@ -3,61 +3,51 @@ define(['jquery'], function($){
 		var self = this;
 		this.callbacks = {
 			render: function(){
-				w_code = self.get_settings().widget_code;
-				self.render_template({
-					caption: {
-						class_name: 'js-ac-caption',
-						html: ''
-					},
-					body: '',
-					render: '\
-					<div class="ac-form-button ac_sub">SEND</div>'
-				});
 				console.log('render');
 				return true;
 			},
 			init: function(){
-				console.log('init');
-				return true;
-			},
-			bind_actions: function(){
-				if (self.system().area == 'llist' || 'clist') {
-					$('.ac-form-button').on('click', function () {
-						var llink = 'https://newdemonew.amocrm.ru/ajax/leads/export/?export_type=csv';
-						var data = self.ldata;
-						console.log(data);
-						var lparam;
-						for (var i=0; i<data.length; i++) {
-							lparam = '&filter[ID][]=' + data[i].id;
-							console.log(lparam);
-							llink += lparam;
-							console.log(llink);
-						}
-						console.log(data);
-						console.log(llink);
-						$.get(llink, function() {
-							console.log('Загрузка завершена.');
-						});
-					});
-				};
-				console.log('bind_actions');
-				return true;
-			},
-			settings: function(){
-				return true;
-			},
-			onSave: function(){
-				alert('click');
-				return true;
-			},
-			destroy: function(){
+      	console.log('init');
+      	return true;
+      },
+      bind_actions: function(){
+      	console.log('bind_actions');
+      	return true;
+      },
+      settings: function(){
+      	return true;
+      },
+      onSave: function(){
+      	alert('click');
+      	return true;
+      },
+      destroy: function(){
 
-			},
-			contacts: {
+      },
+      contacts: {
 					//select contacts in list and clicked on widget name
 					selected: function(){
 						var c_data = self.list_selected().selected;
-						console.log(c_data);
+						$(self.cdata).remove();
+						self.cdata = c_data;
+						// console.log(self.ldata);
+						if (self.cdata) {
+							var clink = 'https://newdemonew.amocrm.ru/ajax/contacts/export/?export_type=csv';
+							var data = self.cdata;
+							// console.log(data);
+							var cparam;
+							for (var i=0; i<data.length; i++) {
+								cparam = '&filter[ID][]=' + data[i].id;
+								// console.log(lparam);
+								clink += cparam;
+								// console.log(llink);
+							}
+							// console.log(data);
+							// console.log(llink);
+							window.open(clink, '_blank');
+						};
+						$('div#card_widgets_overlay').remove();
+						$('div#widgets_block').remove();
 					}
 				},
 				leads: {
@@ -66,7 +56,24 @@ define(['jquery'], function($){
 						var l_data = self.list_selected().selected;
 						$(self.ldata).remove();
 						self.ldata = l_data;
-						console.log(self.ldata);
+						// console.log(self.ldata);
+						if (self.ldata) {
+							var llink = 'https://newdemonew.amocrm.ru/ajax/leads/export/?export_type=csv';
+							var data = self.ldata;
+							// console.log(data);
+							var lparam;
+							for (var i=0; i<data.length; i++) {
+								lparam = '&filter[ID][]=' + data[i].id;
+								// console.log(lparam);
+								llink += lparam;
+								// console.log(llink);
+							}
+							// console.log(data);
+							// console.log(llink);
+							window.open(llink, '_blank');
+						};
+						$('div#card_widgets_overlay').remove();
+						$('div.widgets_block').css('display','none');
 					}
 				},
 				tasks: {
